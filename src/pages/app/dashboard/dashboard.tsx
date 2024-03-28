a43efb0391bad401100aec8375e7650f458b24ea
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2024-03-28 10:41:54 -0300

    Gráfico de receita no período

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import { DayOrdersAmountCard } from './day-orders-amount-card'
 import { MonthCanceledOrdersMountCard } from './month-canceled-orders-amount-card'
 import { MonthOrdersAmountCard } from './month-orders-amount-card'
 import { MonthRevenueCard } from './month-revenue-card'
+import { RevenueChart } from './revenue-chart'
 
 export function Dashboard() {
   return (
@@ -17,6 +18,10 @@ export function Dashboard() {
           <MonthOrdersAmountCard />
           <DayOrdersAmountCard />
           <MonthCanceledOrdersMountCard />
+        </div>
+
+        <div className="grid-col-9 grid gap-4">
+          <RevenueChart />
         </div>
       </div>
     </>
